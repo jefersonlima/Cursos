ba47a577e3761fd72aeeb96e6850fe2e495acc6b
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-12 12:35:08 -0400

    Debuggando com PDB

---
+++ guppe/secao_11-DebugandoETratandoErros/debuggando_com_pdb.py
@@ -0,0 +1,119 @@
+"""
+Debuggando com PDB
+
+PDB -> Python Debugger
+
+Vida de Inseto -> Life's Bug
+
+Bug -> Inseto
+
+#  OBS: A utilização do print() para debugar código é uma prática ruim.
+
+def dividir(a, b):
+    print(f'a={a}, b={b}')
+    try:
+        return int(a) / int(b)
+    except (ValueError, ZeroDivisionError) as err:
+        return f'Ocorreu um Problema: {err}'
+
+print(dividir(4, 7))
+
+#  Existem formas mais profissionais de fazer esse 'debug', utilizando o debugger
+#  Em Python, podemos fazer isso em diferentes IDEs, como o PyCharm ou utilizando
+# o PDB - Python Debugger
+
+#  Exemplo com o PyCharm
+
+def dividir(a, b):
+    try:
+        return int(a) / int(b)
+    except (ValueError, ZeroDivisionError) as err:
+        return f'Ocorreu um Problema: {err}'
+
+print(dividir(4, 0))
+
+#  Exemplo com o PDB - Python Debugger
+
+#  Para utilizar o Python Debugger, precisamos* importar a biblioteca pdb e então utilizar a função set_trace()
+
+#  Comandos básicos do PDB
+#  l (listar onde estamos no código)
+#  n (próxima linha)
+#  p (imprime variável)
+#  c (continua a execução - finaliza o debugging)
+
+import pdb
+
+nome = 'Angelina'
+sobrenome = 'Jolie'
+pdb.set_trace()
+nome_completo = nome + ' ' + sobrenome
+curso = 'Programação em Python: Essencial'
+final = nome_completo + ' faz o curso ' + curso
+print(final)
+
+#  Exemplo com o PDB 2 - Python Debugger
+
+#  Para utilizar o Python Debugger, precisamos* importar a biblioteca pdb e então utilizar a função set_trace()
+
+#  Comandos básicos do PDB
+#  l (listar onde estamos no código)
+#  n (próxima linha)
+#  p (imprime variável)
+#  c (continua a execução - finaliza o debugging)
+
+nome = 'Angelina'
+sobrenome = 'Jolie'
+import pdb; pdb.set_trace()
+nome_completo = nome + ' ' + sobrenome
+curso = 'Programação em Python: Essencial'
+final = nome_completo + ' faz o curso ' + curso
+print(final)
+
+#  Por quê utilizar este formato?
+#  O debug é utilizado durante o desenvolvimento. Costumamos realizar todos os imports de bibliotecas
+#  no ínicio do arquivo. Por isso, ao invés de colocarmos o import do pdb no ínicio do arquivo.
+#  nós colocamos somente onde vamos debuggar, e ao finalizar já fazemos a remoção.
+
+#  Exemplo com o PDB 3 breakpoint - Python Debugger
+
+#  Para utilizar o Python Debugger, precisamos* importar a biblioteca pdb e então utilizar a função set_trace()
+
+#  A partir do Python 3.7, não é mais necessário importar a biblioteca pdb. pois o comando de debug foi
+#  incorporado como função built-in(integrada) chamada breakpoint()
+
+#  Comandos básicos do PDB
+#  l (listar onde estamos no código)
+#  n (próxima linha)
+#  p (imprime variável)
+#  c (continua a execução - finaliza o debugging)
+
+nome = 'Angelina'
+sobrenome = 'Jolie'
+breakpoint()
+nome_completo = nome + ' ' + sobrenome
+curso = 'Programação em Python: Essencial'
+final = nome_completo + ' faz o curso ' + curso
+print(final)
+
+#  OBS: Cuidado com conflitos entre nomes de variaveis e os comandos do pdb
+def soma (l, n, p, c):
+    breakpoint()
+    return l + n + p + c
+
+print(soma(1, 3, 5, 7))
+
+#  Como os nomes das variavéis são os mesmos dos comandos do pdv, devemos utilizat o cimando p para imprimir
+#  as variáveis, Ou seja: p nome_da_variavel
+
+#  Nada de colocar nomes não representativos em variáveis. Sempre optar por nomes significativos.
+
+def soma(num1, num2, num3, num4):
+    breakpoint()
+    return num1 + num2 + num3 + num4
+"""
+
+
+
+
+
