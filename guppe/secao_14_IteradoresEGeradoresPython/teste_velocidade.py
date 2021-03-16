2d2fb3a6bf9af6d369fa6bc915aaedb54c8104c4
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-16 07:11:16 -0400

    Teste de Memória com Generators

---
+++ guppe/secao_14_IteradoresEGeradoresPython/teste_velocidade.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+"""
+Teste de Velocidade com Expressões Geradoras
+
+#  Generators (Geradores)
+
+def nums():
+    for num in range(1, 10):
+        yield num
+
+ge1 = nums()
+
+print(ge1) #  Generator
+
+print(next(ge1))
+print(next(ge1))
+
+#  Generator Expression
+
+ge2 = (num for num in range(1, 10))
+
+print(ge2)
+
+print(next(ge2))
+print(next(ge2))
+
+#  Realizando o teste de velocidade
+import time
+
+#  Generation Expression
+gen_inicio = time.time()
+print(sum(num for num in range(10000000)))
+gen_tempo = time.time() - gen_inicio
+
+#  List Comprehension
+list_inicio = time.time()
+print(sum([num for num in range(10000000)]))
+list_tempo = time.time() - list_inicio
+
+print(f'Generation Expression levou {gen_tempo}')
+print(f'List Comprehension levou {list_tempo}')
+"""
+
+
+
