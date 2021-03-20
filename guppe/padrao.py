b2d8fcfc96a27bddc8003e67a21873f5e2beb9ec
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-20 18:45:25 -0400

    POO - Propriedades - Properties

---
+++ guppe/padrao.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*- #  Configuração para não da problema nos comentário
+"""
+
+
+"""
+#  Classe
+
+    #  Atributos de classe
+
+    #  Construtor
+
+    #  Propriedades (@property = Getters, @nome_atributo.setter = Setters)
+
+    #  Métodos de instancia
