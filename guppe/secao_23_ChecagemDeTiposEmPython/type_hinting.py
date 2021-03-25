417db0ed794f19b3625bcf0eb4b6ecd19cc9bc5c
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-25 06:59:24 -0400

    Type Hinting - Definindo Tipos

---
+++ guppe/secao_23_ChecagemDeTiposEmPython/type_hinting.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*- #  Configuração para não da problema nos comentário
+"""
+Type Hinting - Definindo Tipos
+
+def cumprimentar(nome: str) -> str:
+    return f'Olá, {nome}'
+
+print(cumprimentar('Geek'))
+
+def cabecalho(texto, alinhamento=True):
+    if alinhamento:
+        return f"{texto.title()}\n{'-' * len(texto)}"
+    else:
+        return f"{texto.title()} ".center(50, '#')
+
+print(cabecalho('geek university'))
+print(cabecalho('geek university', alinhamento=False))
+"""
+
+#  Refatorando desafio proposto
+def cabecalho(texto: str, alinhamento: bool = True) -> str:
+    if alinhamento:
+        return f"{texto.title()}\n{'-' * len(texto)}"
+    else:
+        return f"{texto.title()} ".center(50, '#')
+
+print(cabecalho('geek university'))
+print(cabecalho('geek university', alinhamento=False))
