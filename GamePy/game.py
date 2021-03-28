fdc2835c248c189f4bd8bffa71179b2615f81ebb
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-28 18:36:55 -0400

    Implementação Parte 1

--- GamePy/game.py
+++ GamePy/game.py
@@ -1,5 +1,8 @@
+# -*- coding: UTF-8 -*- #  Configuração para não da problema nos comentário1
 """
 Estruturando Game
+
+Implementação Parte 1
 """
 from models.calcular import Calcular
 
@@ -8,7 +11,26 @@ def main() -> None:
     jogar(pontos)
 
 def jogar(pontos: int) -> None:
-    pass
+    dificuldade: int = int(input('Informe o nível de dificuldade desejado [1, 2, 3, ou 4]: '))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a sequinte operação: ')
+
+    calc.mostrar_operacao()
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} ponto(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo? [1 - sim, 0 - não] '))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Você finalizou com {pontos} ponto(s).')
+        print('Até a próxima!')
 
 if __name__ == '__main__':
     main()
