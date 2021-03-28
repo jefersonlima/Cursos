e01b296ea95a4a2d81b003c4dad236aeda896362
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-03-28 10:09:53 -0400

    Estruturando Game

---
+++ GamePy/models/calcular.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*- #  Configuração para não da problema nos comentário
+from random import randint
+
+class Calcular:
+
+    def __init__(self: object, dificuldade: int, /) -> None:
+        self.__dificuldade: int = dificuldade
+        self.__valor1: int = self._gerar_valor
+        self.__valor2: int = self._gerar_valor
+        self.__operacao: int = randint(1, 3) # 1 = somar, 2 = diminuir, 3 = multiplicar
+        self.__resultado: float = self._gerar_resultado
+
+    @property
+    def dificuldade(self: object) -> int:
+        return self.__dificuldade
+
+    @property
+    def valor1(self: object) -> int:
+        return self.__valor1
+
+    @property
+    def valor2(self: object) -> int:
+        return self.__valor2
+
+    @property
+    def operacao(self: object) -> int:
+        return self.__operacao
+
+    @property
+    def resultado(self: object) -> int:
+        return self.__resultado
+
+    @property
+    def _gerar_valor(self:object) -> int:
+        pass
+
+    @property
+    def _gerar_resultado(self: object) -> int:
+        pass
+
+    def checar_resultado(self: object) -> bool:
+        pass
+
+    def mostrar_operacao(self: object) -> None:
+        pass
+
+    def __str__(self: object) -> str:
+        op: str = ''
+        if self.operacao == 1:
+            op = 'Somar'
+        elif self.operacao == 2:
+            op = 'Diminuir'
+        elif self.operacao == 3:
+            op = 'Multiplicar'
+        else:
+            op = 'Operação desconhecida!'
+
+        return f'Valor 1: {self.valor1} \nValor 2: {self.valor2} \nDificuldade: {self.dificuldade} \nOperação: {op}'
---
+++ GamePy/teste.py
@@ -0,0 +1,6 @@
+from models.calcular import Calcular
+
+calc: Calcular = Calcular(1)
+print(calc)
+# calc: Calcular = Calcular(dificuldade=1)
+# print(calc)
